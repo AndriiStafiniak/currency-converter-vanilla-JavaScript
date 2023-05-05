17ccf38a371669d45c80b9bfeb5410ee4ffca762
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-05-05 12:40:26 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+{
+   const calculateResult = (currency, pln) => {
+      const currentExchangeRateEuro = 4.62;
+      const currentExchangeRateDolar = 4.21;
+      switch (currency) {
+         case "EUR":
+            return pln / currentExchangeRateEuro;
+         case "USD":
+            return pln / currentExchangeRateDolar;
+      }
+   };
+
+   const updateResultText = (result, currency) => {
+      const resultElement = document.querySelector(".js-result");
+      resultElement.innerText = `${result.toFixed(2)} ${currency}`;
+   };
+
+   const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      const plnElement = document.querySelector(".js-pln");
+      const currencyElement = document.querySelector(".js-currency");
+      const currency = currencyElement.value
+      let result = calculateResult(currency, plnElement.value);
+      updateResultText(result, currency);
+   };
+
+   const init = () => {
+      const formElement = document.querySelector(".js-form");
+
+      formElement.addEventListener("submit", onFormSubmit);
+   };
+   init();
+}
